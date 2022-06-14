e07dc3e00125e7d9489ba8c88501ba3ced449f88
max_etf <max_etf@mail.ru>
2022-06-15 00:59:35 +0500

    ready

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,19 +1,11 @@
 const counter = document.getElementById("clicker__counter");
 const image = document.getElementById("cookie");
 
-function cookieclicker() {
+image.onclick = function () {
+    let timerId = null;
     image.width = 220;
     image.heigth = 220;
     counter.textContent++;
-    setInterval(()=>{image.width =200; image.heigth = 200 },50)
+    timerId = setTimeout(()=>{image.width =200; image.heigth = 200; clearTimeout(timerId) },100) //а как сбросить timerID ?
 };
 
-
-image.onclick = cookieclicker;
-
-
-/* console.log(counter);
-console.log(image);
-console.log(counter.textContent);
-counter.textContent = 199;
-console.log(counter.textContent); */
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,23 +1,63 @@
 let successClickCounterString = document.getElementById('dead');
 let failClickCounterString = document.getElementById('lost');
+let totalholes = document.querySelectorAll('.hole').length;
 let successClickCounter = parseInt(successClickCounterString.textContent);
 let failClickCounter = parseInt(failClickCounterString.textContent);
-/* function checkMole (hole) {
-    if (hole.classList.contains('hole hole_has-mole')){
-        successClickCounter++
-    }
-    else {
-        failClickCounter++
-    }
-} */
 
-
-
-if ((successClickCounter < 10)||(failClickCounter<5)) {
+/* 
+// window-click 
+ if ((successClickCounter < 10)||(failClickCounter<5)) {
     window.onclick = element => { 
         console.log(element);
-        if (element.target.classList.contains('hole_has-mole')) {successClickCounter++; successClickCounterString.textContent = successClickCounter}
-        else {failClickCounter++; failClickCounterString.textContent = failClickCounter}}
+        if (element.target.classList.contains('hole_has-mole')) {
+            successClickCounter++;
+             successClickCounterString.textContent = successClickCounter
+             if(successClickCounter >= 10) {
+                  alert('pobed');
+                  failClickCounter = 0;
+                  successClickCounter = 0;
+                  successClickCounterString.textContent = successClickCounter;
+                  failClickCounterString.textContent = failClickCounter;
+                }
+            }
+        else {
+            failClickCounter++;
+            failClickCounterString.textContent = failClickCounter;
+            if(failClickCounter >= 5) { 
+                alert('loose');
+                failClickCounter = 0;
+                successClickCounter = 0;
+                successClickCounterString.textContent = successClickCounter;
+                failClickCounterString.textContent = failClickCounter;
+            };
+        }
+    }
+}  */
+
+// ну или так, хотя хотелось бы компактнее
+for (let i = 1; i < totalholes+1; i++) {
+    document.getElementById('hole'+i).onclick = function () {
+        if (this.classList.contains('hole_has-mole')) {
+            successClickCounter++;
+            successClickCounterString.textContent = successClickCounter;
+        }
+        else {
+            failClickCounter++;
+            failClickCounterString.textContent = failClickCounter;
+        }
+        if(successClickCounter >= 10) {
+            alert('pobed');
+            failClickCounter = 0;
+            successClickCounter = 0;
+            successClickCounterString.textContent = successClickCounter;
+            failClickCounterString.textContent = failClickCounter;
+          }
+          if(failClickCounter >= 5) { 
+            alert('loose');
+            failClickCounter = 0;
+            successClickCounter = 0;
+            successClickCounterString.textContent = successClickCounter;
+            failClickCounterString.textContent = failClickCounter;
+          }
+    }    
 }
-else if(successClickCounter >= 10) alert('pobed')
-else if(failClickCounter >= 5) alert('loose')
