f0b71e23370d131206bda9d69b3595ee443a15e8
max_etf <max_etf@mail.ru>
2022-06-24 19:22:22 +0500

    task1 ready

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,7 +68,15 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
+      let result = [];      
+      let options = (Array.from(this.input.querySelectorAll('option')));
+      options.forEach(element => {
+        if (element.text.includes(text)){
+          result.push({text:(element.text),value:(element.value)})
+        }
+      });
+    
+      /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
       Метод должен вернуть массив.
@@ -81,12 +89,7 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return result
   }
 }
 
